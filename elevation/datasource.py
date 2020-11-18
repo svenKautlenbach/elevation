d865483d3fd6579f5358acac7be0423aabd47917
Alessandro Amici <a.amici@bopen.eu>
2020-11-18 10:07:33 +0100

    shorten length of compatibility imports (isort and black now agree)

--- elevation/datasource.py
+++ elevation/datasource.py
@@ -15,8 +15,7 @@
 # limitations under the License.
 
 # python 2 support via python-future
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import absolute_import, division, unicode_literals
 
 import collections
 import math
